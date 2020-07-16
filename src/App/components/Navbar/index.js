4cd964330fb347d50867c107c1ee646d74f1c2f8
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:15:11 -0300

    navbar component

---
+++ src/App/components/Navbar/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useMediaPredicate } from "react-media-hook";
+import logoBranco from './image/FHT_Branco.png'
+import menu from './image/menu-24px.png'
+import close from './image/close-24px.png'
+import {container, linkStyle, containerMobile} from './style'
+
+const Navbar = ({links}) => {
+    const [hamburger, setHamburger] = useState(false);
+    const webLayout = useMediaPredicate("(min-width: 600px)")
+	return (
+        <div style={{height:'100%'}}>
+            {
+                webLayout ? (
+                    <div style={container}>
+                        <a href="/"><img src='https://uploads-ssl.webflow.com/5e8bee16fa583a22b3cfbae6/5e8bee16fa583a3beacfbb63_sigalei-logo-branca.png' alt="Logo" style={{width:'120px', margin:'10px'}}/></a>
+                        <div style={{display:'flex'}}>
+                            {links.map((link, index) => {
+                                return (
+                                    <div key={index} style={{alignSelf:'center'}}>
+                                        <a style={linkStyle} href={link.href}>{link.title}</a>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                ):(
+                    hamburger ? (
+                        <>
+                        <div style={containerMobile}>
+                            <a style={{color:'white', position:'fixed', top:'0'}} onClick={() => setHamburger(false)}><img src={close} alt="Fechar menu" style={{width:'30px', margin:'5px'}}/></a>
+                            <img src={logoBranco} alt="Logo" style={{width:'70px', margin:'10px auto', marginTop:'40px'}}/>
+                            <div style={{display:'flex', flexDirection:'column', marginTop:'40px'}}>
+                                {links.map((link,index) => {
+                                    return (
+                                        <div key={index} style={{alignSelf:'center'}}>
+                                            <a style={linkStyle} href={link.href}>{link.title}</a>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        </>
+                    ):(
+                        <div>
+                            <a style={{overflow: 'hidden',position:'fixed',top:0}} onClick={() => setHamburger(true)}><img src={menu} alt="Menu Hamburger" style={{width:'30px', margin:'5px'}}/></a>
+                        </div>
+                    )
+                )
+            }   
+        </div>
+	)
+}
+
+export default Navbar
---
+++ src/App/components/Navbar/style.js
@@ -0,0 +1,34 @@
+export const
+    container = {
+        overflow: 'hidden',
+        position:'fixed',
+        top:0,
+        display:'flex', 
+        justifyContent:'space-between',
+        width: '100%',
+        heigth:'80px',
+        zIndex:'200',
+        backgroundColor:'#011C53',
+        shadow:'2px 2px 2px 1px #FAEAFF',
+        boxShadow:'0 2px 3px 1px #B3B3B3'
+    },
+    linkStyle = {
+        textDecoration:'none',
+        fontSize:'16px',
+        color:'white',
+        padding:'20px'
+    },
+    containerMobile={
+        overflow: 'hidden',
+        position:'fixed',
+        top:0,
+        display:'flex',
+        flexDirection:'column',
+        textAlign:'center',
+        width: '80%',
+        height:'100%',
+        zIndex:'200',
+        backgroundColor:'#011C53',
+        shadow:'2px 2px 2px 1px #FAEAFF',
+        boxShadow:'0 2px 3px 1px #B3B3B3'
+    }
