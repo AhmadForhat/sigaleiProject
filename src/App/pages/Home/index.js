5bbebab6d77aaaad4a6ff7867212df9549d788b6
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:16:42 -0300

    home page

---
+++ src/App/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Chamada from '../../components/Chamada'
+
+const Home = () => {
+	return (
+		<>
+			<Chamada />
+		</>
+	)
+}
+
+export default Home
