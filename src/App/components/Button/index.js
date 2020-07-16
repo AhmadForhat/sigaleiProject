e7e6ee30195629cf35d11b52f7e3aac0134d08e7
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:08:52 -0300

    button component

---
+++ src/App/components/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useMediaPredicate } from 'react-media-hook'; 
+import {buttonLink, buttonLinkMobile, buttonForm} from './style'
+
+const Button = ({type, title, funcao}) => {
+    const webLayout = useMediaPredicate("(min-width: 600px)")
+    if(type === 'submit') return (
+    <button style={{width:'40%', margin:'0 auto'}} onClick={() => funcao}>{title}</button>
+    )
+    if(type === 'link') return(
+        <a href={funcao} style={{width:'100%', display:'flex'}}><button style={webLayout ? buttonLink: buttonLinkMobile}>{title}</button></a>
+    )
+    if(type === 'form') return(
+        <button type='submit' style={buttonForm} onClick={funcao}>{title}</button>
+    )
+}
+
+export default Button
---
+++ src/App/components/Button/style.js
@@ -0,0 +1,34 @@
+export const
+    buttonLink = {
+        width:'15%',
+        borderRadius:'8px',
+        minWidth:'100px',
+        padding:'10px',
+        margin:'0 auto',
+        marginTop:'20px',
+        border:'none',
+        backgroundColor:'#1E1447',
+        color:'white'
+    },
+    buttonLinkMobile = {
+        width:'15%',
+        borderRadius:'4px',
+        fontSize:'14px',
+        minWidth:'120px',
+        padding:'5px',
+        margin:'0 auto',
+        marginTop:'20px',
+        border:'none',
+        backgroundColor:'#1E1447',
+        color:'white'
+    },
+    buttonForm={
+        width:'100%',
+        borderRadius:'8px',
+        minWidth:'100px',
+        padding:'10px',
+        margin:'0 auto',
+        border:'none',
+        backgroundColor:'#1E1447',
+        color:'white'
+    }
