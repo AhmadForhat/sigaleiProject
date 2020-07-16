be18ca714af0b5de4a3dc13a5adf79e8376ec7cb
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:09:33 -0300

    componente chamada

---
+++ src/App/components/Chamada/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useMediaPredicate } from "react-media-hook";
+import { motion } from 'framer-motion'
+import {containerWeb, containerMobile} from './style'
+import imgChamada from './image/devChamada.svg'
+import Button from '../Button'
+
+const Chamada = () => {
+    const webLayout = useMediaPredicate("(min-width: 600px)")
+	return (
+        <div style={webLayout ? containerWeb : containerMobile}>
+            <div style={{display:'flex', justifyContent:'space-around', alignItems:'center', flexWrap:'wrap', marginBottom:'50px'}}>
+                <h1 style={{fontSize:'2.3em', maxWidth:'400px', width:'80%', color:'#011C53'}}>Veja as informações do Github da sua empresa com facilidade!</h1>
+                <img src={imgChamada} alt="imagem de chamada inicial" style={{width:'40%', minWidth:'250px', alignSelf:'center', zIndex:'2'}}/>
+            </div>
+            <Button type='link' title='Cadastre-se' funcao='/cadastro'/>
+        </div>
+    )
+}
+
+export default Chamada
---
+++ src/App/components/Chamada/style.js
@@ -0,0 +1,14 @@
+export const
+    containerWeb = {
+        width:'100%',
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginTop:'100px'
+    },
+    containerMobile = {
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginTop:'50px'
+    }
