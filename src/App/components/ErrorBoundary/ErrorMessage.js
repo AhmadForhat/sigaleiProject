78e86a9a8b763e82454ede9b26b207f57b8cb20c
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:10:17 -0300

    componente errorMessage

---
+++ src/App/components/ErrorBoundary/ErrorMessage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ErrorMessage = () =>
+	<div style={{margin:'0 auto'}}>
+		<h1 style={{color:'red'}}>Erro na página</h1>
+		<p>Ocorreu um erro na renderização</p>
+		<p>Recarregue a página ou contate o suporte</p>
+	</div>
+
+export default ErrorMessage
