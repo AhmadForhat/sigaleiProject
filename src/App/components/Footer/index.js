3fe24090cbc6974653cfa63aa977ad3a79b47e00
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:11:19 -0300

    footer component

---
+++ src/App/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import {container} from './style'
+import imgFacebook from './image/facebook.png'
+import imgInstagram from './image/instagram.png'
+import imgLinkedin from './image/linkedin.png'
+
+const Footer = () => {
+    return (
+        <div style={container}>
+            <h2 style={{alignSelf:'center'}}>Todos direitos Reservador para Sigalei</h2>
+            <div style={{alignSelf:'center'}}>
+                <a href="https://www.facebook.com/" target='blank'><img style={{width:'60px'}} src={imgFacebook} alt='logo facebook'/></a>
+                <a href="https://www.instagram.com/" target='blank'><img style={{width:'60px'}} src={imgInstagram} alt='logo instagram'/></a>
+                <a href="https://www.linkedin.com/" target='blank'><img style={{width:'60px'}} src={imgLinkedin} alt='logo linkedin'/></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/App/components/Footer/style.js
@@ -0,0 +1,8 @@
+export const
+    container = {
+        display:'flex',
+        color:'white',
+        width:'100%',
+        backgroundColor:'#011C53',
+        justifyContent:'space-around'
+    }
