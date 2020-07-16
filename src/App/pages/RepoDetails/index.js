fd3936ec5eec9a9b6220390e1cce4d557c9423cf
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-07-16 20:18:56 -0300

    page repo details

---
+++ src/App/pages/RepoDetails/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GraphicCommits from '../../components/GraphicCommits'
+
+const RepoDetails = ({data}) => {
+    const [repoInfo] = data
+    return (
+        <div style={{height: '100%',display:'flex', flexDirection:'column', width:'100%', justifyContent:'center', marginTop:'80px'}}>
+            <a style={{alignSelf:'center'}} href={repoInfo.url}>{repoInfo.name}</a>
+            <div style={{height:'400px', width:'90%', alignSelf:'center'}}>
+                <GraphicCommits data={repoInfo}/>
+            </div>
+        </div>
+    )
+}
+
+export default RepoDetails
